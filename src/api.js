e3d3e9dbef077e1851636729489219342cd016fb
Junior Miksza <junior.miksza8@gmail.com>
2021-03-03 19:52:38 -0300

    env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const {
+  REACT_APP_API_URL
+} = process.env
+
 const api = axios.create({
-  baseURL : ''
+  baseURL: REACT_APP_API_URL || 'http://localhost:3000'
 })
 
 export default api;
